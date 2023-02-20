d8b56664e3f2e6194041a8f41a2d1b669f6099bf
NicolaPlacentino <nyko0921@gmail.com>
2023-02-20 15:39:38 +0100

    added base html structure

--- index.php
+++ index.php
@@ -24,7 +24,32 @@ icona della categoria ed il tipo di articolo che si sta visualizzando (prodotto,
 </head>
 
 <body>
-    
+<div class="container py-4">
+    <div class="row row-cols-4">
+
+        <?php for($i = 0; $i < 8; $i++) : ?>
+            <div class="col mb-5">
+                <div class="card" style="width: 18rem;">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMQOO5zqXXp4li2ktQigiM2jqLjmxbiXRoQw&usqp=CAU" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h4 class="card-title">Card title</h4>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div>
+                                <h5>Price</h5>
+                                <p>Category</p>
+                            </div>
+                            <div class="text-center pe-3">
+                                <span class="fs-1"><i class="fa-solid fa-dog"></i></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <?php endfor; ?>
+        
+    </div>
+</div>
 </body>
 
 </html>
